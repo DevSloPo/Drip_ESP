cae25de7a2927b70a9d7427217d8097b38b1ce70
DevSloPo <1342087454@qq.com>
2025-05-05 17:29:10 +0800

    Add files via upload

---
+++ Settings.lua
@@ -0,0 +1,11 @@
+--ESPlibrary 丨 No obf
+--BY du78
+
+return {
+    TextColor = Color3.new(0, 1, 1),
+    OutlineColor = Color3.new(0, 1, 1),
+    TextSize = 15,
+    ModelName = "target_model",
+    HighlightName = "Drip_Highlight",
+    CheckForHumanoid = true
+}
